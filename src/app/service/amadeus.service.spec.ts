318f8b90d2236dae1b6bd093607d4a5f8a6f9cbd
Shahid khan <shahidcub201@gmail.com>
2023-02-09 11:52:29 +0600

    Update amadeus.service.spec.ts

--- src/app/service/amadeus.service.spec.ts
+++ src/app/service/amadeus.service.spec.ts
@@ -3,7 +3,7 @@ import { of } from 'rxjs';
 import '@angular/compiler';
 import { AmadeusService } from './amadeus.service';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
+
 import { Injectable, ReflectiveInjector} from '@angular/core';
 import { Airport } from '../interfaces/airport';
 import {TestBed} from '@angular/core/testing'
@@ -20,7 +20,6 @@ const airportsMocks = [
 let apiClient: HttpClient
 describe('AmadeusService', () => {
 
-=======
 import { Injectable, ReflectiveInjector } from '@angular/core';
 import { Airport } from '../interfaces/airport';
 import { TestBed } from '@angular/core/testing';
@@ -39,12 +38,12 @@ const airportsMocks = [
 ];
 let apiClient: HttpClient;
 describe('AmadeusService', () => {
->>>>>>> b4ed4300b0003f593616bdf5bbefb82e4fdb6b29
+
   const http = { get: jest.fn(() => of(airportsMocks)) };
   let service: AmadeusService;
   const provide = (mock: any): any => mock;
   service = new AmadeusService(provide(http) as any);
-<<<<<<< HEAD
+
 
 
   test('should fetch a list of airports', () => {
@@ -58,15 +57,7 @@ describe('AmadeusService', () => {
     service.airportRoute('dhaka').subscribe((airports) => {
     
       expect(http.get).toHaveBeenCalled()
-=======
-  test('should fetch a list of airports', () => {
-    let result = service.airportSearch('dhaka').subscribe((airports) => {
-      // expect(http.get).toHaveBeenCalled();
-      expect(http.get).toHaveBeenCalledWith(
-        'http://localhost:3000/aiport/dhaka'
-      );
-    });
-  });
+
   it('should fetch a list of airports with given iata code', () => {
     service.airportRoute('dhaka').subscribe((airports) => {
       console.log(airports);
@@ -76,15 +67,11 @@ describe('AmadeusService', () => {
   it('should fetch a list of options', () => {
     service.searchFlight('dhaka').subscribe((airports) => {
       console.log(airports);
->>>>>>> b4ed4300b0003f593616bdf5bbefb82e4fdb6b29
+
     });
   });
 
-  it('should fetch a list of options', () => {
-    service.searchFlight('dhaka').subscribe((airports) => {
-     
-    });
-  });
+  
 
   test('should Call the airport route with iataCode', () => {
     let result = service.airportRoute('DAC').subscribe((airports) => {
